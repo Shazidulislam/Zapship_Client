a5bb082f65e488b6e68cc6a7172b5786bd3df58e
Shazidul islam <shazidulislam910@gmail.com>
2025-11-27 20:10:53 +0800

    covarage map section

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -19,6 +19,7 @@ const router = createBrowserRouter(
         {
           path:"coverage",
           Component:Coverage,
+          loader:()=> fetch("./servicesCenter.json")
         }
     ]
    },
